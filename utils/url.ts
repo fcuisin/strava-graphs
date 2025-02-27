b891cc184015b17c82f7e709b06962470a826f2a
Ferdinand Cuisin <ferdinand.c@meta-api.io>
2025-02-27 16:09:48 +0100

    fix(url): update redirect app url.

--- utils/url.ts
+++ utils/url.ts
@@ -1,1 +1,1 @@
-export const STRAVA_OAUTH_URL = `https://www.strava.com/oauth/authorize?client_id=${process.env.STRAVA_CLIENT_ID}&redirect_uri=http://localhost:3000/api/auth&response_type=code&%20response_type=force&scope=activity:read`;
+export const STRAVA_OAUTH_URL = `https://www.strava.com/oauth/authorize?client_id=${process.env.STRAVA_CLIENT_ID}&redirect_uri=${process.env.BASE_URL}/api/auth&response_type=code&%20response_type=force&scope=activity:read`;
